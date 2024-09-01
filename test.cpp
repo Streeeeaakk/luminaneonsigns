bfda9ea39edec06158aedb56a4f45fc7c967a92c
Jullian Lance Mañas <s2020925@usls.edu.ph>
2024-09-01 20:47:57 +0800

    custom liquid code

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    string s;
+
+    // Take input using cin
+    cin >> s;
+
+    // Print output
+    cout << s;
+
+    return 0;
+}
